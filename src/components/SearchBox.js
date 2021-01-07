9a200bb22e01a8ea712b5708d1b0b928264e1ba6
spreet2472 <samre2472@gmail.com>
2021-01-07 12:46:25 -0500

    searchBox

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,12 +1,22 @@
+import axios from 'axios';
 function SearchBox() {
-  const handleClick = (e) => {
-    console.log(e);
-  };
+  const url = `http://api.tvmaze.com/singlesearch/shows?q=girls`;
+  async function getData() {
+    try {
+      const response = await axios.get(url);
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  }
   return (
-    <div className='searchBox'>
-      <input type='search' placeholder='Search for Shows...'></input>
-      <button onClick={(e) => handleClick(e.target)}>Search</button>
-    </div>
+    <>
+      <div className='searchBox'>
+        <input type='search' placeholder='Search for Shows...'>
+          {getData}
+        </input>
+      </div>
+    </>
   );
 }
 export default SearchBox;
