0286215354ffda0e267a97cf3d58e2d1a32c78a7
spreet2472 <samre2472@gmail.com>
2021-01-27 12:49:14 -0500

    WorkingApi

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 import Navbar from './components/Navbar';
 import SearchBox from './components/SearchBox';
 
 function App() {
+  // const [error, setError] = useState(null);
+  // const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('http://api.tvmaze.com/search/shows?q=girls')
+      .then((response) => setItems(response.data));
+    // .then(
+    //   (data) => {
+    //     setIsLoaded(true);
+    //     setItems(data);
+    //   },
+    //   (error) => {
+    //     setIsLoaded(true);
+    //     setError(error);
+    //   }
+    // );
+  }, [items]);
+
   return (
     <div className='App'>
       <Navbar />
       <div className='content'>
         <SearchBox />
+        <ul>
+          {items &&
+            items.map((item) => (
+              <li key={item.id}>
+                <img
+                  className='.image-card'
+                  src={item.show.image.medium}
+                  alt=''
+                ></img>
+              </li>
+            ))}
+        </ul>
       </div>
     </div>
   );
--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,20 +1,8 @@
-import axios from 'axios';
 function SearchBox() {
-  const url = `http://api.tvmaze.com/singlesearch/shows?q=girls`;
-  async function getData() {
-    try {
-      const response = await axios.get(url);
-      console.log(response);
-    } catch (error) {
-      console.error(error);
-    }
-  }
   return (
     <>
       <div className='searchBox'>
-        <input type='search' placeholder='Search for Shows...'>
-          {getData}
-        </input>
+        <input type='search' placeholder='Search for Shows...'></input>
       </div>
     </>
   );
